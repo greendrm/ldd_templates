6b3c32d62a5cd96f1d84eecd29c62111c162e80f
greendrm <greendrm@gmail.com>
2011-07-17 15:21:50 +0900

    license: add license template

---
+++ drivers/license/license.c
@@ -0,0 +1,14 @@
+/* drivers/input/touch/sample-i2c-client.c
+ *
+ * Copyright (C) 2011 Dojip Kim, inc.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that is will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABLILITY of FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Genernal Public License for more details.
+ *
+ */
