f200b5b11499eacf30879a42555053bf1c18f3d4
greendrm <greendrm@gmail.com>
2011-07-17 15:20:46 +0900

    I2C: update the license

--- drivers/i2c/sample-i2c-client-board.c
+++ drivers/i2c/sample-i2c-client-board.c
@@ -1,3 +1,18 @@
+/* arch/arm/mach-omap2/board-xxx.c
+ *
+ * Copyright (C) 2011 Dojip Kim.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that is will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABLILITY of FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Genernal Public License for more details.
+ *
+ */
+
 #include <linux/kernel.h>
 #include <linux/i2c.h>
 
